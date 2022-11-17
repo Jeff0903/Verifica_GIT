b2cc552bb23430ceeda41fda3b03a819d952bdd5
Giacomo Mughetti Maraldi <GIACOMO.MUGHETTI@local.ittcesena.it>
2022-11-17 09:09:57 +0100

    Creazione classe Biblioteca

---
+++ Prova_Pratica_GIT/Biblioteca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Pratica_GIT
+{
+    internal class Biblioteca
+    {
+        internal string nomeBiblioteca { get; set; }
+        internal string indirizzo { get; set; }
+        internal string orarioApertura { get; set; }
+        internal string orarioChiusura { get; set; }
+        
+    }
+}
